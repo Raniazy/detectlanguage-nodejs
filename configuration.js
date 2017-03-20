26707129e7348e5215d307fadb2fdd4d3a03637b
Rania ZYANE <rania.zyane@gmail.com>
2017-03-20 14:55:47 +0000

    Init configuration file

---
+++ configuration.js
@@ -0,0 +1,6 @@
+module.exports = {
+	method : "detect",
+    API_KEY : "d66420d11af9b8db81f599813c717aa0",
+    baseUrlDetect : "http://ws.detectlanguage.com/0.2/",
+    baseUrlLanguages : "http://ws.detectlanguage.com/0.2/languages"
+};
