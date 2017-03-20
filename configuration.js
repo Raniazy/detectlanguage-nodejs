d4d0b987ec0a8eb0723da0531d972378db0f43c1
Rania ZYANE <rania.zyane@gmail.com>
2017-03-20 15:05:29 +0000

    Update configuration file

--- configuration.js
+++ configuration.js
@@ -1,6 +1,6 @@
 module.exports = {
 	method : "detect",
-    API_KEY : "YOUR_API_ACCESS_KEY",
-    baseUrlDetect : "http://ws.detectlanguage.com/0.2/",
-    baseUrlLanguages : "http://ws.detectlanguage.com/0.2/languages"
+	API_KEY : "YOUR_API_ACCESS_KEY",
+	baseUrlDetect : "http://ws.detectlanguage.com/0.2/",
+	baseUrlLanguages : "http://ws.detectlanguage.com/0.2/languages"
 };
